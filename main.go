02876566df3755d6ae200aa53a21b92bb0eabe61
19rthompson <19rsthompson@gmail.com>
2024-04-15 14:27:33 -0600

    updated worker.go to match Russ's starter code

--- main.go
+++ main.go
@@ -1,22 +1,175 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
+type MapTask struct {
+	M, R       int    // total number of map and reduce tasks
+	N          int    // map task number, 0-based
+	SourceHost string // address of host with map input file
+}
+
+type ReduceTask struct {
+	M, R        int      // total number of map and reduce tasks
+	N           int      // reduce task number, 0-based
+	SourceHosts []string // addresses of map workers
+}
+
+type Pair struct {
+	Key   string
+	Value string
+}
+
+type Interface interface {
+	Map(key, value string, output chan<- Pair) error
+	Reduce(key string, values <-chan string, output chan<- Pair) error
+}
+
+func mapSourceFile(m int) string       { return fmt.Sprintf("map_%d_source.db", m) }
+func mapInputFile(m int) string        { return fmt.Sprintf("map_%d_input.db", m) }
+func mapOutputFile(m, r int) string    { return fmt.Sprintf("map_%d_output_%d.db", m, r) }
+func reduceInputFile(r int) string     { return fmt.Sprintf("reduce_%d_input.db", r) }
+func reduceOutputFile(r int) string    { return fmt.Sprintf("reduce_%d_output.db", r) }
+func reducePartialFile(r int) string   { return fmt.Sprintf("reduce_%d_partial.db", r) }
+func reduceTempFile(r int) string      { return fmt.Sprintf("reduce_%d_temp.db", r) }
+func makeURL(host, file string) string { return fmt.Sprintf("http://%s/data/%s", host, file) }
+
+func getLocalAddress() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	localaddress := localAddr.IP.String()
+
+	if localaddress == "" {
+		panic("init: failed to find non-loopback interface with valid address on this node")
+	}
+	return localaddress
+}
+
+type Client struct{}
+
+func (c Client) Map(key, value string, output chan<- Pair) error {
+	defer close(output)
+	lst := strings.Fields(value)
+	for _, elt := range lst {
+		word := strings.Map(func(r rune) rune {
+			if unicode.IsLetter(r) || unicode.IsDigit(r) {
+				return unicode.ToLower(r)
+			}
+			return -1
+		}, elt)
+		if len(word) > 0 {
+			output <- Pair{Key: word, Value: "1"}
+		}
+	}
+	return nil
+}
+
+func (c Client) Reduce(key string, values <-chan string, output chan<- Pair) error {
+	defer close(output)
+	count := 0
+	for v := range values {
+		i, err := strconv.Atoi(v)
+		if err != nil {
+			return err
+		}
+		count += i
+	}
+	p := Pair{Key: key, Value: strconv.Itoa(count)}
+	output <- p
+	return nil
+}
+
 func main() {
-    splitDatabase("austen.db", []string{"austen-0.db", "austen-1.db", "austen-2.db", "austen-3.db", "austen-4.db"})
-    tempdir := ""
-    address := "127.0.0.1:5000"
-    go func() {
-    http.Handle("/data/", http.StripPrefix("/data", http.FileServer(http.Dir(tempdir))))
-    if err := http.ListenAndServe(address, nil); err != nil {
-        log.Printf("Error in HTTP server for %s: %v", address, err)
-    }
-}()
-    _, err := mergeDatabases([]string{"http://127.0.0.1:5000/data/austen-0.db","http://127.0.0.1:5000/data/austen-1.db","http://127.0.0.1:5000/data/austen-2.db","http://127.0.0.1:5000/data/austen-3.db","http://127.0.0.1:5000/data/austen-4.db"}, "austen2.db", "tempy")
-    if err != nil {
-        log.Printf("Error merging databases: %v", err)
-    }
+	m := 10
+	r := 5
+	source := "source.db"
+	//target := "target.db"
+	tmp := os.TempDir()
+
+	tempdir := filepath.Join(tmp, fmt.Sprintf("mapreduce.%d", os.Getpid()))
+	if err := os.RemoveAll(tempdir); err != nil {
+		log.Fatalf("unable to delete old temp dir: %v", err)
+	}
+	if err := os.Mkdir(tempdir, 0700); err != nil {
+		log.Fatalf("unable to create temp dir: %v", err)
+	}
+	defer os.RemoveAll(tempdir)
+
+	log.Printf("splitting %s into %d pieces", source, m)
+	var paths []string
+	for i := 0; i < m; i++ {
+		paths = append(paths, filepath.Join(tempdir, mapSourceFile(i)))
+	}
+	if err := splitDatabase(source, paths); err != nil {
+		log.Fatalf("splitting database: %v", err)
+	}
+
+	myAddress := net.JoinHostPort(getLocalAddress(), "3410")
+	log.Printf("starting http server at %s", myAddress)
+	go func() {
+		http.Handle("/data/", http.StripPrefix("/data", http.FileServer(http.Dir(tempdir))))
+		if err := http.ListenAndServe(myAddress, nil); err != nil {
+			log.Fatalf("Error in HTTP server for %s: %v", myAddress, err)
+		}
+	}()
+
+	// build the map tasks
+	var mapTasks []*MapTask
+	for i := 0; i < m; i++ {
+		task := &MapTask{
+			M:          m,
+			R:          r,
+			N:          i,
+			SourceHost: myAddress,
+		}
+		mapTasks = append(mapTasks, task)
+	}
+
+	// build the reduce tasks
+	var reduceTasks []*ReduceTask
+	for i := 0; i < r; i++ {
+		task := &ReduceTask{
+			M:           m,
+			R:           r,
+			N:           i,
+			SourceHosts: make([]string, m),
+		}
+		reduceTasks = append(reduceTasks, task)
+	}
+
+	var client Client
+
+	// process the map tasks
+	for i, task := range mapTasks {
+		if err := task.Process(tempdir, client); err != nil {
+			log.Fatalf("processing map task %d: %v", i, err)
+		}
+		for _, reduce := range reduceTasks {
+			reduce.SourceHosts[i] = myAddress
+		}
+	}
+
+	// process the reduce tasks
+	for i, task := range reduceTasks {
+		if err := task.Process(tempdir, client); err != nil {
+			log.Fatalf("processing reduce task %d: %v", i, err)
+		}
+	}
+
+	// gather outputs into final target.db file
 }

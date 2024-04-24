76b9e6e318d438755674e908f7ab8099b6896cf2
WhitingJarod <whitingjarod@gmail.com>
2024-04-24 10:11:08 -0600

    now downloading directories badly

--- main.go
+++ main.go
@@ -1,37 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
-	"unicode"
+    "fmt"
+    "log"
+    "net"
+    "net/http"
+    "os"
+    "path/filepath"
+    "strconv"
+    "strings"
+    "unicode"
 )
 
+func LOG(format string, args ...interface{}) {
+    log.Printf(format, args...)
+}
+
+func ERR(format string, args ...interface{}) {
+    log.Printf(format, args...)
+}
+
 type MapTask struct {
-	M, R       int    // total number of map and reduce tasks
-	N          int    // map task number, 0-based
-	SourceHost string // address of host with map input file
+    NumMapTasks, NumReduceTasks int    // total number of map and reduce tasks
+    TaskId                      int    // map task number, 0-based
+    SourceHost                  string // address of host with map input file
 }
 
 type ReduceTask struct {
-	M, R        int      // total number of map and reduce tasks
-	N           int      // reduce task number, 0-based
-	SourceHosts []string // addresses of map workers
+    NumMapTasks, NumReduceTasks int      // total number of map and reduce tasks
+    TaskId                      int      // reduce task number, 0-based
+    SourceHosts                 []string // addresses of map workers
 }
 
 type Pair struct {
-	Key   string
-	Value string
+    Key   string
+    Value string
 }
 
 type Interface interface {
-	Map(key, value string, output chan<- Pair) error
-	Reduce(key string, values <-chan string, output chan<- Pair) error
+    Map(key, value string, output chan<- Pair) error
+    Reduce(key string, values <-chan string, output chan<- Pair) error
 }
 
 func mapSourceFile(m int) string       { return fmt.Sprintf("map_%d_source.db", m) }
@@ -44,132 +52,141 @@ func reduceTempFile(r int) string      { return fmt.Sprintf("reduce_%d_temp.db",
 func makeURL(host, file string) string { return fmt.Sprintf("http://%s/data/%s", host, file) }
 
 func getLocalAddress() string {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
 
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-	localaddress := localAddr.IP.String()
+    localaddress := localAddr.IP.String()
 
-	if localaddress == "" {
-		panic("init: failed to find non-loopback interface with valid address on this node")
-	}
-	return localaddress
+    if localaddress == "" {
+        panic("init: failed to find non-loopback interface with valid address on this node")
+    }
+    return localaddress
 }
 
 type Client struct{}
 
 func (c Client) Map(key, value string, output chan<- Pair) error {
-	defer close(output)
-	lst := strings.Fields(value)
-	for _, elt := range lst {
-		word := strings.Map(func(r rune) rune {
-			if unicode.IsLetter(r) || unicode.IsDigit(r) {
-				return unicode.ToLower(r)
-			}
-			return -1
-		}, elt)
-		if len(word) > 0 {
-			output <- Pair{Key: word, Value: "1"}
-		}
-	}
-	return nil
+    defer close(output)
+    lst := strings.Fields(value)
+    for _, elt := range lst {
+        word := strings.Map(func(r rune) rune {
+            if unicode.IsLetter(r) || unicode.IsDigit(r) {
+                return unicode.ToLower(r)
+            }
+            return -1
+        }, elt)
+        if len(word) > 0 {
+            output <- Pair{Key: word, Value: "1"}
+        }
+    }
+    return nil
 }
 
 func (c Client) Reduce(key string, values <-chan string, output chan<- Pair) error {
-	defer close(output)
-	count := 0
-	for v := range values {
-		i, err := strconv.Atoi(v)
-		if err != nil {
-			return err
-		}
-		count += i
-	}
-	p := Pair{Key: key, Value: strconv.Itoa(count)}
-	output <- p
-	return nil
+    defer close(output)
+    count := 0
+    for v := range values {
+        i, err := strconv.Atoi(v)
+        if err != nil {
+            return err
+        }
+        count += i
+    }
+    p := Pair{Key: key, Value: strconv.Itoa(count)}
+    output <- p
+    return nil
 }
 
 func main() {
-	m := 10
-	r := 5
-	source := "source.db"
-	//target := "target.db"
-	tmp := os.TempDir()
+    m := 10
+    r := 5
+    source := "source.db"
+    //target := "target.db"
+    tmp := os.TempDir()
 
-	tempdir := filepath.Join(tmp, fmt.Sprintf("mapreduce.%d", os.Getpid()))
-	if err := os.RemoveAll(tempdir); err != nil {
-		log.Fatalf("unable to delete old temp dir: %v", err)
-	}
-	if err := os.Mkdir(tempdir, 0700); err != nil {
-		log.Fatalf("unable to create temp dir: %v", err)
-	}
-	defer os.RemoveAll(tempdir)
+    tempdir := filepath.Join(tmp, fmt.Sprintf("mapreduce.%d", os.Getpid()))
+    if err := os.RemoveAll(tempdir); err != nil {
+        log.Fatalf("unable to delete old temp dir: %v", err)
+    }
+    if err := os.Mkdir(tempdir, 0700); err != nil {
+        log.Fatalf("unable to create temp dir: %v", err)
+    }
+    defer os.RemoveAll(tempdir)
 
-	log.Printf("splitting %s into %d pieces", source, m)
-	var paths []string
-	for i := 0; i < m; i++ {
-		paths = append(paths, filepath.Join(tempdir, mapSourceFile(i)))
-	}
-	if err := splitDatabase(source, paths); err != nil {
-		log.Fatalf("splitting database: %v", err)
-	}
+    log.Printf("splitting %s into %d pieces", source, m)
+    var paths []string
+    for i := 0; i < m; i++ {
+        paths = append(paths, filepath.Join(tempdir, mapSourceFile(i)))
+    }
+    if err := splitDatabase(source, paths); err != nil {
+        log.Fatalf("splitting database: %v", err)
+    }
 
-	myAddress := net.JoinHostPort(getLocalAddress(), "3410")
-	log.Printf("starting http server at %s", myAddress)
-	go func() {
-		http.Handle("/data/", http.StripPrefix("/data", http.FileServer(http.Dir(tempdir))))
-		if err := http.ListenAndServe(myAddress, nil); err != nil {
-			log.Fatalf("Error in HTTP server for %s: %v", myAddress, err)
-		}
-	}()
+    myAddress := net.JoinHostPort(getLocalAddress(), "3410")
+    log.Printf("starting http server at %s", myAddress)
+    listener, err := net.Listen("tcp", myAddress)
+    http.Handle("/data/", http.StripPrefix("/data", http.FileServer(http.Dir(tempdir))))
+    if err != nil {
+        log.Fatalf("Listen error on address %s: %v", myAddress, err)
+    }
+    go func() {
+        if err := http.Serve(listener, nil); err != nil {
+            log.Fatalf("Serve error: %v", err)
+        }
+    }()
+    // go func() {
+    //     http.Handle("/data/", http.StripPrefix("/data", http.FileServer(http.Dir(tempdir))))
+    //     if err := http.ListenAndServe(myAddress, nil); err != nil {
+    //         log.Fatalf("Error in HTTP server for %s: %v", myAddress, err)
+    //     }
+    // }()
 
-	// build the map tasks
-	var mapTasks []*MapTask
-	for i := 0; i < m; i++ {
-		task := &MapTask{
-			M:          m,
-			R:          r,
-			N:          i,
-			SourceHost: myAddress,
-		}
-		mapTasks = append(mapTasks, task)
-	}
+    // build the map tasks
+    var mapTasks []*MapTask
+    for i := 0; i < m; i++ {
+        task := &MapTask{
+            NumMapTasks:    m,
+            NumReduceTasks: r,
+            TaskId:         i,
+            SourceHost:     myAddress,
+        }
+        mapTasks = append(mapTasks, task)
+    }
 
-	// build the reduce tasks
-	var reduceTasks []*ReduceTask
-	for i := 0; i < r; i++ {
-		task := &ReduceTask{
-			M:           m,
-			R:           r,
-			N:           i,
-			SourceHosts: make([]string, m),
-		}
-		reduceTasks = append(reduceTasks, task)
-	}
+    // build the reduce tasks
+    var reduceTasks []*ReduceTask
+    for i := 0; i < r; i++ {
+        task := &ReduceTask{
+            NumMapTasks:    m,
+            NumReduceTasks: r,
+            TaskId:         i,
+            SourceHosts:    make([]string, m),
+        }
+        reduceTasks = append(reduceTasks, task)
+    }
+    var client Client
 
-	var client Client
+    // process the map tasks
+    for i, task := range mapTasks {
+        if err := task.Process(tempdir, client); err != nil {
+            log.Fatalf("processing map task %d: %v", i, err)
+        }
+        for _, reduce := range reduceTasks {
+            reduce.SourceHosts[i] = myAddress
+        }
+    }
 
-	// process the map tasks
-	for i, task := range mapTasks {
-		if err := task.Process(tempdir, client); err != nil {
-			log.Fatalf("processing map task %d: %v", i, err)
-		}
-		for _, reduce := range reduceTasks {
-			reduce.SourceHosts[i] = myAddress
-		}
-	}
+    // process the reduce tasks
+    for i, task := range reduceTasks {
+        if err := task.Process(tempdir, client); err != nil {
+            log.Fatalf("processing reduce task %d: %v", i, err)
+        }
+    }
 
-	// process the reduce tasks
-	for i, task := range reduceTasks {
-		if err := task.Process(tempdir, client); err != nil {
-			log.Fatalf("processing reduce task %d: %v", i, err)
-		}
-	}
-
-	// gather outputs into final target.db file
+    // gather outputs into final target.db file
 }
--- worker.go
+++ worker.go
@@ -2,55 +2,121 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"hash/fnv"
 )
 
-
-type MapTask struct {
-    M, R int // total number of map and reduce tasks
-    N int // map task number, 0-based
-    SourceHost string // address of host with map input files
-}
-
-type ReduceTask struct {
-    M, R int // total number of map and reduce tasks
-    N int // reduce task number, 0-based
-    SourceHosts []string // addresses of map workers
-}
-
-type Pair struct {
-    Key string
-    Value string
-}
-
-type Interface interface {
-    Map(key, value string, output chan<-Pair) error
-    Reduce(key string, values <-chan string, output chan<-Pair) error
-}
-
-func mapSourceFile(m int) string { return fmt.Sprintf("map_%d_source.db", m) }
-func mapInputFile(m int) string { return fmt.Sprintf("map_%d_input.db", m) }
-func mapOutputFile(m, r int) string { return fmt.Sprintf("map_%d_output_%d.db", m, r) }
-func reduceInputFile(r int) string { return fmt.Sprintf("reduce_%d_input.db", r) }
-func reduceOutputFile(r int) string { return fmt.Sprintf("reduce_%d_output.db", r) }
-func reducePartialFile(r int) string { return fmt.Sprintf("reduce_%d_partial.db", r) }
-func reduceTempFile(r int) string { return fmt.Sprintf("reduce_%d_temp.db", r) }
-func makeURL(host, file string) string { return fmt.Sprintf("http://%s/data/%s", host, file) }
-
 func (task *MapTask) Process(tempdir string, client Interface) error {
-    // Download and open the input file
-    sourceURL := makeURL(task.SourceHost, mapSourceFile(task.N))
-    db, err := downloadDatabase(sourceURL, tempdir)
-    if err != nil {
-        return err
-    }
-    // Create the output files using createDatabase
-    outputDBs := make([]*sql.DB, task.R)
-    for r := 0; r < task.R; r++ {
-        outputDB, err := createDatabase(mapOutputFile(task.N, r))
-        if err != nil {
-            return err
-        }
-        outputDBs[r] = outputDB
-    }
+	// Download and open the input file
+	sourceURL := makeURL(task.SourceHost, mapSourceFile(task.TaskId))
+	db, err := downloadDatabase(sourceURL, tempdir)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	// Create the output files using createDatabase
+	outputDBs := make([]*sql.DB, task.NumReduceTasks)
+	for r := 0; r < task.NumReduceTasks; r++ {
+		outputDB, err := createDatabase(mapOutputFile(task.TaskId, r))
+		if err != nil {
+			return err
+		}
+		outputDBs[r] = outputDB
+		defer outputDB.Close()
+	}
+
+	rows, err := db.Query("select key, value from pairs")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	coco_channels := make([]chan Pair, 0)
+	total_rows := 0
+	for rows.Next() {
+		total_rows++
+		var key, value string
+		if err := rows.Scan(&key, &value); err != nil {
+			return err
+		}
+		channel := make(chan Pair)
+		coco_channels = append(coco_channels, channel)
+		go client.Map(key, value, channel)
+	}
+	LOG("Input rows: %d", total_rows)
+
+	total_pairs := 0
+	for _, channel := range coco_channels {
+		// while channel is open
+		for pair := range channel {
+			total_pairs++
+			hash := fnv.New32()
+			hash.Write([]byte(pair.Key))
+			target := int(hash.Sum32() % uint32(task.NumReduceTasks))
+			outputDB := outputDBs[target]
+			outputDB.Exec("insert into pairs (key, value) values (?, ?)", pair.Key, pair.Value)
+		}
+	}
+	LOG("Output pairs: %d", total_pairs)
+	ERR("There was no error, and that is sus")
+	return nil
+}
+
+func (task *ReduceTask) Process(tempdir string, client Interface) error {
+	// Open the input files
+	inputDBs := make([]*sql.DB, task.NumMapTasks)
+	for m := 0; m < task.NumMapTasks; m++ {
+		db, err := openDatabase(mapOutputFile(m, task.TaskId))
+		if err != nil {
+			return err
+		}
+		inputDBs[m] = db
+		defer db.Close()
+	}
+	// Create the output file using createDatabase
+	outputDB, err := createDatabase(reduceOutputFile(task.TaskId))
+	if err != nil {
+		return err
+	}
+	defer outputDB.Close()
+
+	coco_channels := make([]chan Pair, 0)
+	total_rows := 0
+	for _, db := range inputDBs {
+		rows, err := db.Query("select key, value from pairs order by key, value")
+		if err != nil {
+			return err
+		}
+		defer rows.Close()
+		var input chan string
+		prev_key := ""
+		for rows.Next() {
+			total_rows++
+			var key, value string
+			if err := rows.Scan(&key, &value); err != nil {
+				return err
+			}
+			if key != prev_key {
+				if input != nil {
+					close(input)
+				}
+				input = make(chan string)
+				out_channel := make(chan Pair)
+				go client.Reduce(key, input, out_channel)
+				prev_key = key
+			}
+			input <- value
+		}
+	}
+	LOG("Output rows: %d", total_rows)
+	total_pairs := 0
+	for _, channel := range coco_channels {
+		// while channel is open
+		for pair := range channel {
+			total_pairs++
+			outputDB.Exec("insert into pairs (key, value) values (?, ?)", pair.Key, pair.Value)
+		}
+	}
+	LOG("Output pairs: %d", total_pairs)
+	ERR("There was no error, and that is sus")
+	return nil
 }
